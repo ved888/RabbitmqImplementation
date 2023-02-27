683aadade213d28b354ee545f9514cc33087e978
ved888 <vedprakashv888@gmail.com>
2023-02-27 01:31:41 -0800

    In this code is defined rabbitmq code.

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	fmt.Println("RabbitMQ in Golang: Getting started tutorial")
+
+	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer connection.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ instance")
+}
